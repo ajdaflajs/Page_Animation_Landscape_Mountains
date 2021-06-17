8553dde1e5f66a7b586ccf20c9122e748e08bce5
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2021-06-17 14:08:42 +0200

    animation update
    heroImage: height and width animation;
    slider; logoText and mountain logo animation

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const logoText = document.querySelector("#logoText");
 const mountain = document.querySelector(".mountain");
 const headline = document.querySelector(".headline");
 
+
+/* Timeline animation (tl = timeline) */
+
 const tl = new TimelineMax();
 
-/* Timeline animation */
+tl.fromTo(heroImage, 0.8, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 
-tl.fromTo(heroImage, 0.9, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(heroImage, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
 
-.fromTo(heroImage, 1.2, {width: "100%"},{width: "80%", ease: Power2.easeInOut});
+.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.4")
 
-.fromTo()
+.fromTo(logoText, 0.5, { opacity: 0, x: 30}, { opacity: 1, x: 0}, "-=0.5")
 
-/* if we want more than one animations on the picture, we should not close the tl.fromTo with (;) but add more .fromTo elements */
+.fromTo(mountain, 0.5, { opacity: 0, x: 30}, { opacity: 1, x: 0}, "-=0.5");
+
+/* if we want more than one animations on the picture, we should not close the tl.fromTo with (;) but add more .fromTo elements in between */
+
+/* "-=seconds" means that the animation will start sooner, the same amount of seconds as specified */
