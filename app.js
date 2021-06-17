2fceec3979367653b05b8749f7adfffa14a17cdf
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2021-06-17 13:12:38 +0200

    const creation
    const creation in app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const heroImage = document.querySelector(".heroImage");
+const slider = document.querySelector(".slider");
+const logoText = document.querySelector("#logoText");
+const mountain = document.querySelector(".mountain");
+const headline = document.querySelector(".headline");
