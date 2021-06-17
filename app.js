e741c0fa493cb76d41741026c19abbc9dc5fbfab
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2021-06-17 13:23:14 +0200

    height heroImage animation

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const slider = document.querySelector(".slider");
 const logoText = document.querySelector("#logoText");
 const mountain = document.querySelector(".mountain");
 const headline = document.querySelector(".headline");
+
+const tl = new TimelineMax();
+
+tl.fromTo(heroImage, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
